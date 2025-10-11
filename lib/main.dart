913daf3802cc2421bdfdde80c16bca317758390d
arqluk <alejandro.lukacher@gmail.com>
2025-10-11 17:40:41 -0300

    Elimino banner debugMode

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Welcome to Car Rent'),
